8ccb2cfb58fa244fff71973497bb80d89af70de7
Meet Dave <meet.dave@outlook.com>
2020-04-22 17:39:10 +0200

    Final commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,10 @@
-import imgArr from "../assets/*.svg";
-const maxTotalScore = 8; // Keep it multiple of 4 as per design
+import imgArr from "../assets/*.svg"; // Get all the images name in the build
+const maxTotalScore = 8; // Keep it multiple of 4 as per UI design
 const array = [...Array(maxTotalScore).keys()];
-const duplicateArrayLength = [...array, ...array];
-let finalCardsArray = duplicateArrayLength.sort(() => Math.random() - Math.random());
+const duplicateArrayLength = [...array, ...array]; // Since we need a pair of two, we will clone each element. 
+let finalCardsArray = duplicateArrayLength.sort(() => Math.random() - Math.random()); // Shuffle the sequence of array
 
+// All those global vars! Pheew
 let cardsObjects = [];
 let turns = [];
 let turnsCount = 0;
@@ -12,14 +13,14 @@ let html;
 let clickListener = true;
 
 const itemsRenderer = (itemsArray) => {
-  cardsObjects = itemsArray;
   let markup = new Array();
+  cardsObjects = itemsArray; // This is the most recent state of out object
   itemsArray.map(i => {
     html = `    
-        <div id="${i.key}" class="flex-item">
-          <img id="${i.key}" class="item-img" src="${imgArr[i.show ? i.img : 'joker']}" />
-        </div>
-        `;
+      <div id="${i.key}" class="flex-item">
+        <img id="${i.key}" class="item-img" src="${imgArr[i.show ? i.img : 'joker']}" />
+      </div>
+    `;
     markup.push(html);
     html = markup.join("");
   });
@@ -28,10 +29,10 @@ const itemsRenderer = (itemsArray) => {
   clickListener = true;
 }
 
-// const toggleDisplay = (i) => {
-//   return { ...cardsObjects[turns[0]], show: !cardsObjects[turns[0]].show };
-// }
+const toggleShow = i => ({ ...cardsObjects[i], show: !cardsObjects[i].show });
 
+const won = i => ({ ...cardsObjects[i], show: true, won: true })
+  
 const init = () => {
   finalCardsArray.map((c, key) => {
     cardsObjects[key] = {
@@ -43,75 +44,47 @@ const init = () => {
   });
 
   itemsRenderer(cardsObjects);
+}
 
-  document.addEventListener('click', function (event) {
-    if (clickListener && event.target && event.target.id && cardsObjects[event.target.id]) {
-      let i = event.target.id;
-      turns = [...turns, i];
+// Listen to click events
+document.addEventListener('click', function (event) {
+  if (clickListener && event.target && event.target.id && cardsObjects[event.target.id]) {
+    let i = event.target.id;
+    turns = [...turns, i];
 
-      if (cardsObjects[i].won === true) return;
+    if (cardsObjects[i].won === true) return; // Don't entertain the click if the pair is already won.
 
-      if (turns.length === 1) {
-        toggleDisplay(turns[0]);
-        cardsObjects[i] = {
-          ...cardsObjects[turns[0]],
-          show: !cardsObjects[turns[0]].show
-        };
-        
-        itemsRenderer(cardsObjects);
-        return;
-      }
+    if (turns.length === 1) {
+      cardsObjects[i] = toggleShow(turns[0]);        
+      return itemsRenderer(cardsObjects);
+    }
+    
+    if (turns.length === 2) {
+      turnsCount++;
       
-      if (turns.length === 2) {
-        turnsCount++;
-        if (cardsObjects[turns[0]].img === cardsObjects[turns[1]].img) {
-          score++;
+      if (cardsObjects[turns[0]].img === cardsObjects[turns[1]].img) {
+        score++;
 
-          cardsObjects[turns[0]] = {
-            ...cardsObjects[turns[0]],
-            show: true,
-            won: true
-          };
+        cardsObjects[turns[0]] = won(turns[0]);
+        cardsObjects[turns[1]] = won(turns[1]);
 
-          cardsObjects[turns[1]] = {
-            ...cardsObjects[turns[1]],
-            show: true,
-            won: true
-          };
-
-          alert(`Yaaay, that's a point!! Score: ${score}, Turns: ${turnsCount}`);
-
-          turns = [];
-          itemsRenderer(cardsObjects);
-        } else {
-
-          cardsObjects[i] = {
-            ...cardsObjects[i],
-            show: !cardsObjects[i].show
-          };
-          
-          itemsRenderer(cardsObjects);
-
-          const res = cardsObjects.map(c => c.won === true ? {
-            ...c,
-            show: true
-          } : {
-            ...c,
-            show: false
-          });
-          
-          turns = [];
-          clickListener = false; // Don't listen for clicks until the cards are reset
-          setTimeout(() => itemsRenderer(res), 2000);
-        }
+        alert(`Yaaay, that's a point!! Score: ${score}, Turns played: ${turnsCount}`);
+        
+        turns = [];
+        return itemsRenderer(cardsObjects);
+      } else {
+        cardsObjects[i] = toggleShow(i);
+        itemsRenderer(cardsObjects); // It didn't match, but we still need to show the card
+        const res = cardsObjects.map(c => c.won === true ? { ...c, show: true } : { ...c, show: false }); // Only need to show set of cards that have won
+        turns = [];
+        clickListener = false; // Don't listen for clicks until the cards are reset
+        setTimeout(() => itemsRenderer(res), 2000);
       }
     }
-  
-    // Don't follow the link
-    event.preventDefault();
-  
-  }, false);
+  }
 
-}
+  event.preventDefault(); // Don't follow the link
+
+}, false);
 
 init();
